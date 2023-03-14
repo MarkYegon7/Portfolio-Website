3a82a91384df5cc961aa0bab709e35ec639318b7
MarkYegon7 <mark.yegon@student.moringaschool.com>
2023-03-14 20:25:17 +0300

    Menu dropdown

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let hamburgerMenu = document.querySelector('.image-icon');
-let desktopMenu = document.querySelector('.mobile-Menu-list');
+let desktopMenu = document.querySelector('.name');
+let backGround = document.querySelector('.mobile-Menu-list')
 
 function clickOn () {
   hamburgerMenu.classList.add("hambDisplay");
   desktopMenu.classList.add('mobile-Menu-list-display');
+  backGround.classList.add("imageBackGround")
 }
 hamburgerMenu.addEventListener('click', clickOn);
