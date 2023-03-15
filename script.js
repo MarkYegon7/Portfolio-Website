8c4f99503bf613ed3ec1b8ed43c18da7577b6341
MarkYegon7 <mark.yegon@student.moringaschool.com>
2023-03-15 18:47:51 +0300

    Add javaScript file

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let hamburgerMenu = document.querySelector('.image-icon');
-let desktopMenu = document.querySelector('.name');
-let backGround = document.querySelector('.mobile-Menu-list')
+const hamburgerMenu = document.querySelector('.image-icon');
+const desktopMenu = document.querySelector('.name-1');
+const clickButton = document.querySelector('#close_button');
+const navigationNav = document.querySelectorAll('.mobile-Menu-list > li > a');
 
-function clickOn () {
-  hamburgerMenu.classList.add("hambDisplay");
-  desktopMenu.classList.add('mobile-Menu-list-display');
-  backGround.classList.add("imageBackGround")
+function clickOn() {
+  desktopMenu.style.display = 'flex';
 }
+
+function closeMenu() {
+  desktopMenu.style.display = 'none';
+}
+
 hamburgerMenu.addEventListener('click', clickOn);
+clickButton.addEventListener('click', closeMenu);
+navigationNav.forEach((each) => each.addEventListener('click', closeMenu));
